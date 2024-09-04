66e0688067e94a6b174fe219fda8c50d4bf18a9a
Hernawan Faiz Abdillah <hernawan.faiz.abdillah@gmail.com>
2024-09-04 09:42:23 +0700

    chore: use unsafe channel by default

--- run-client.sh
+++ run-client.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-opcua-commander -e opc.tcp://localhost:4855 -u sample1 -p sample1pwd -P=Basic256Sha256 -s SignAndEncrypt -c ./pki/own/cert.der
+# opcua-commander -e opc.tcp://localhost:4855 -u sample1 -p sample1pwd -P=Basic256Sha256 -s SignAndEncrypt -c ./pki/own/cert.der
+opcua-commander -e opc.tcp://127.0.0.1:4855 #-P=None -s None #-u sample1 -p sample1pwd -v
+# opcua-commander -e opc.tcp://localhost:4855/ -P=None -s None -v
